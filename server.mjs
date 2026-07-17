fcbe415a1e73fe314cdbab0e7834656153ba5c28
Ronald Fernandes <ronaldfernandes17@hotmail.com>
2026-07-16 21:04:45 -0300

    feat: adicionar interface para recurso de usuarios

--- server.mjs
+++ server.mjs
@@ -1,5 +1,27 @@
 import http from 'node:http';
 
+const users = [{
+    name: 'Ronald Fernandes',
+    age: 24,
+    status: true
+}, {
+    name: 'valdiana bessa',
+    age: 31,
+    status: true
+}, {
+    name: 'Ane Bessa',
+    age: 0,
+    status: false
+}];
+
+const USERSJSON = JSON.stringify(users);
+
 http.createServer((request, response) => {
-    response.end('hello client')
+    if (request.url === '/users') {
+        response.writeHead(200, { 'content-type': 'application/json' });
+        response.end(JSON.stringify(users));
+}   else {
+    response.writeHead(404, { 'content-type': 'application/json' });
+        response.end(JSON.stringify({message : 'pagina nao encontrada'}));
+    }
 }).listen(3000);
